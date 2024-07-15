1c91d1849d0d16d559989c531ca18382728be785
rockcym <129655538+rockcym@users.noreply.github.com>
2024-07-16 00:55:14 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -318,7 +318,7 @@ const BLOG = {
      {
       name: '我记得-赵雷',
       artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=974443814.mp3',
+      url: 'https://music.163.com/song/media/outer/url?id=1974443814.mp3',
       cover:
         'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
     },
