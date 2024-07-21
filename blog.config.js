6a4e6ac6212fd17e1dd674b08a4d00f0510bce5e
rockcym <129655538+rockcym@users.noreply.github.com>
2024-07-22 00:32:53 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -309,9 +309,9 @@ const BLOG = {
   MUSIC_PLAYER_AUDIO_LIST: [
     // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
     {
-      name: '风を共に舞う気持ち',
+      name: 'Opening Live',
       artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
+      url: 'https://music.163.com/song/media/outer/url?id=27867424.mp3',
       cover:
         'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
     },
@@ -336,6 +336,20 @@ const BLOG = {
       cover:
         'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
     },
+        {
+      name: '落日海面-执剑稳',
+      artist: 'Falcom Sound Team jdk',
+      url: 'https://music.163.com/song/media/outer/url?id=1330348068.mp3',
+      cover:
+        'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+    },
+	{
+      name: '起风了-买辣椒也用券_原曲：《ヤキモチ》—高桥优',
+      artist: 'Falcom Sound Team jdk',
+      url: 'https://music.163.com/song/media/outer/url?id=2139948465.mp3',
+      cover:
+        'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+    },  
     {
       name: '王都グランセル',
       artist: 'Falcom Sound Team jdk',
